62e2c2ac30a9efc94edf60d0a0cd13133a134ee3
Yokeshwaran S <144541937+Yokeshwaran1411@users.noreply.github.com>
2025-07-22 19:32:14 +0530

    Update vite.config.js

--- ChatApp/clientSide/vite.config.js
+++ ChatApp/clientSide/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/chatapp/',
+  base: './', // 👈 add this line
   plugins: [react(), tailwindcss()],
 });
